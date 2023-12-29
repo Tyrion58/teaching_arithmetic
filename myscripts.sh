1f7785be786d5d97d1f4535abef7893af91375cd
tunyu <947775182@qq.com>
2023-12-29 09:59:44 +0800

    complete label training data

--- myscripts.sh
+++ myscripts.sh
@@ -4,3 +4,6 @@ python sample.py --out_dir=out/out-addition-no-bal
 # 训练balanced addition
 python train.py config/babygpt/train_addition_bal.py
 nohup python train.py config/babygpt/train_addition_bal.py &
+
+# 训练balanced label addition
+python train.py config/babygpt/train_addition_label.py
